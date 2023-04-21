4845b7ddbe487088761c80340efdc9e4a7e53582
Nisha Chaurasiya <125041085+Nisha16C@users.noreply.github.com>
2023-04-21 15:32:54 +0530

    chnges

--- mysql-connector.py
+++ mysql-connector.py
@@ -9,55 +9,55 @@ mydb = mysql.connector.connect(
 )
 
 
-#..................................Insert Data..........................................................................
+# ..................................Insert Data..........................................................................
 
-#
-# mycursor = mydb.cursor()
-# mysql = "INSERT INTO users(id, username, password) VALUES({}, '{}', '{}')".format(5, 'shiv', 'vihs')
-# mycursor.execute(mysql)
-# mydb.commit()
-# print(mysql)
-# print("user saved to db")
+
+mycursor = mydb.cursor()
+mysql = "INSERT INTO users(id, username, password) VALUES({}, '{}', '{}')".format(5, 'shiv', 'vihs')
+mycursor.execute(mysql)
+mydb.commit()
+print(mysql)
+print("user saved to db")
 
 
 # ............................................Fetch data ...............................................................
 
-#
-# mycursor = mydb.cursor()
-# mycursor.execute('SELECT * FROM users');
-#
-# users = mycursor.fetchall()
-#
-# for user in users:
-#     # print(users)
-#     print('user_id', "=", user[0])
-#     print('username', "=", " "  + user[1])
-#     print('password', "=", " " + user[2])
+
+mycursor = mydb.cursor()
+mycursor.execute('SELECT * FROM users');
+
+users = mycursor.fetchall()
+
+for user in users:
+    # print(users)
+    print('user_id', "=", user[0])
+    print('username', "=", " "  + user[1])
+    print('password', "=", " " + user[2])
 
 
 
-#.......................................................Delete data.....................................................
+# .......................................................Delete data.....................................................
 
 
-# mycursor = mydb.cursor()
-# mysql = "DELETE FROM users WHERE username = ('{}')".format('Nisha')
-# mycursor.execute(mysql)
-# mydb.commit()
-# print(mysql)
-# print("user Delete to db")
+mycursor = mydb.cursor()
+mysql = "DELETE FROM users WHERE username = ('{}')".format('Nisha')
+mycursor.execute(mysql)
+mydb.commit()
+print(mysql)
+print("user Delete to db")
 
 
 
 
-#.......................................................Update data.....................................................
+# .......................................................Update data.....................................................
 
-#
-# mycursor = mydb.cursor()
-# mysql = "UPDATE users SET username='{}', password='{}' WHERE id={}".format('Actor', 'rotcA', 4)
-# mycursor.execute(mysql)
-# mydb.commit()
-# print(mysql)
-# print("user updated to db")
+
+mycursor = mydb.cursor()
+mysql = "UPDATE users SET username='{}', password='{}' WHERE id={}".format('Actor', 'rotcA', 4)
+mycursor.execute(mysql)
+mydb.commit()
+print(mysql)
+print("user updated to db")
 
 
 
